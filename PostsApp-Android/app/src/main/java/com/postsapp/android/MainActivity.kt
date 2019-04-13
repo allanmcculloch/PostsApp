1128fe475fd80b148e730618cdcf7c59725536e7
Allan McCulloch <allan@carnallan.com>
2019-04-13 13:31:52 +0100

    Improve use of navigation component and tidy up layouts

--- PostsApp-Android/app/src/main/java/com/postsapp/android/MainActivity.kt
+++ PostsApp-Android/app/src/main/java/com/postsapp/android/MainActivity.kt
@@ -4,31 +4,26 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        navigation.setOnNavigationItemSelectedListener(this)
+        val nav = findNavController(R.id.nav_host_fragment)
+
+        bottom_navigation.setupWithNavController(nav)
+        toolbar.setupWithNavController(nav)
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-
-        when (item.itemId) {
-            R.id.menu_posts -> {
-                findNavController(R.id.nav_host_fragment).navigate(R.id.postsListFragment)
-            }
-            R.id.menu_users -> {
-                findNavController(R.id.nav_host_fragment).navigate(R.id.usersFragment)
-            }
-            R.id.menu_comments -> {
-              //  findNavController(R.id.nav_host_fragment).navigate(R.id.commentsFragment)
-            }
-        }
-        return true
+        val navController = findNavController(R.id.nav_host_fragment)
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
 }
