0d1a3ebdcf8745469abcf0a9fd0e2974533445e9
Allan McCulloch <allan@carnallan.com>
2019-04-13 19:19:24 +0100

    Add company to user list

--- PostsApp-Android/app/src/main/java/com/postsapp/android/ui/userslist/UserViewModel.kt
+++ PostsApp-Android/app/src/main/java/com/postsapp/android/ui/userslist/UserViewModel.kt
@@ -9,15 +9,17 @@ class UserViewModel {
     private val _userName : MutableLiveData<String> = MutableLiveData()
     private val _email : MutableLiveData<String> = MutableLiveData()
     private val _imageUrl : MutableLiveData<String> = MutableLiveData()
+    private val _company : MutableLiveData<String> = MutableLiveData()
 
     val userName: LiveData<String> = _userName
     val email: LiveData<String> = _email
     val imageUrl: LiveData<String> = _imageUrl
+    val company: LiveData<String> = _company
 
     fun bind(user : User) {
-
         _userName.value = user.name
         _email.value = user.email
         _imageUrl.value = "$AVATAR_BASE_URL/${user.id}.png"
+        _company.value = user.company?.name
     }
 }
--- PostsApp-Android/app/src/test/java/com/postsapp/android/ui/userslist/UserViewModelTest.kt
+++ PostsApp-Android/app/src/test/java/com/postsapp/android/ui/userslist/UserViewModelTest.kt
@@ -3,6 +3,7 @@ package com.postsapp.android.ui.userslist
 import com.postsapp.android.ui.BaseViewModelTest
 import com.postsapp.android.model.User
 import com.jraska.livedata.test
+import com.postsapp.android.model.Company
 import org.junit.Test
 
 class UserViewModelTest : BaseViewModelTest() {
@@ -16,9 +17,10 @@ class UserViewModelTest : BaseViewModelTest() {
         viewModel.userName.test().assertValue(userTestData.name)
         viewModel.imageUrl.test().assertValue("https://api.adorable.io/avatars/285/${userTestData.id}.png")
         viewModel.email.test().assertValue(userTestData.email)
+        viewModel.company.test().assertValue(userTestData.company?.name)
     }
 
     private fun createViewModel() = UserViewModel()
 
-    private val userTestData = User(1, "user1", "email@email.com")
+    private val userTestData = User(1, "user1", "email@email.com",company = Company("some company","catchphrase","bs"))
 }
